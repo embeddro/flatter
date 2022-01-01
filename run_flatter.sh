cd2cc8b6129491d4063e1560ad418e961c9ec695
Aleksei Prusakov <aleksey.prus@gmail.com>
2022-01-01 19:13:17 +0300

    change run_flatter.sh

--- run_flatter.sh
+++ run_flatter.sh
@@ -1,2 +1,9 @@
-#!/usr/bin/bash
-docker run --env-file ./env_file --name avito_yar flatter avito_spider -a region='yaroslavl'
+#!/bin/bash
+
+for region in $(cat regions);do
+	echo "Запускаем парсер $region"
+	docker run --env-file ./env_file \
+	       	   --name "avito_$region" \
+		   flatter avito_spider \
+		   -a region=$region 2>&1 | tee "res_$region"
+done
